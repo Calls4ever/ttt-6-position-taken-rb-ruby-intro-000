f53e19c0e9f2d590275ac21ec09307712616b1e1
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-09 02:33:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken(board, index)
   if board[index.to_i-1]!=" "
     return false
-  elsif  board[input.to_i-1]=="X" || board[input.to_i-1]!="O"
+  elsif  board[index.to_i-1]=="X" || board[input.to_i-1]!="O"
     return true
   end
 end
