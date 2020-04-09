15af8ca44177192259d609bfb31adc5973e552f7
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-09 02:33:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
-def position_taken(board, input)
-  if board[input.to_i-1]!=" "
+def position_taken(board, index)
+  if board[index.to_i-1]!=" "
     return false
   elsif  board[input.to_i-1]=="X" || board[input.to_i-1]!="O"
     return true
