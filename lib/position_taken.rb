fff13df8fb42f0ee2e24bd4662ef12c05fe75e95
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-09 02:48:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if (board[index.to_i-1]!=" " || board[index.to_i-1]!="")
+  if (board[index.to_i]!=" " || board[index.to_i]!="")
     return false
-  elsif (board[index.to_i-1]=="X" || board[input.to_i-1]=="O")
+  elsif (board[index.to_i]=="X" || board[input.to_i]=="O")
     return true
 
   end
